0596252b5f8bc1a3bd1dc4a1ac439447dd79c5c2
Denowich <denowich@gmail.com>
2022-07-02 19:23:20 +0300

    created server js and add dev dependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import images from "./api-mock.json";
 
 function App() {
-  return <div>Hello React!</div>;
+  const [imageList, setImageList] = useState(images.resources);
+  return (
+    <div className="image-grid">
+      {imageList.map((image) => (
+        <img src={image.url} alt={image.public_id}></img>
+      ))}
+    </div>
+  );
 }
 
 export default App;
